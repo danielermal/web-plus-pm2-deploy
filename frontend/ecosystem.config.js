01c255d91645fa3153e9fc3653fd2dcfb1f8cf81
danielermal <ermalyukd@yandex.ru>
2023-02-23 23:07:46 +0700

    deploy

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/danielermal/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
