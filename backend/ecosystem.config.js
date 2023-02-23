fc8b67b0f0d6f54b0b4f31e07801f36c2b0d4467
danielermal <ermalyukd@yandex.ru>
2023-02-23 23:38:12 +0700

    deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,8 +18,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/danielermal/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./.env ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'pre-deploy-local': `scp ./.env ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd frontend && npm i && npm run build',
     },
   },
 };
